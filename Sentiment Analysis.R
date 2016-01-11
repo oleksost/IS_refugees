9f83d93c0023661a9b46e4d58886cc9d6843e1c1
oleksost <oleks.ostapenko@t-online.de>
2016-01-11 11:56:20 +0100

    deleted to big data

--- Sentiment Analysis.R
+++ Sentiment Analysis.R
@@ -1,8 +1,98 @@
 setwd("~/Documents/HU Berlin/WI 1516/refugeestest/IS refugees")
 #reading data
 data.refugees<-read.csv(file ="Data/Data_refugees.csv")
-
 #Find seperate words for santiment
+
+score.sentiment = function(sentences, pos.words, neg.words, exc.words, .progress='none')
+{
+  require(plyr)
+  require(stringr)
+  
+  # we got a vector of sentences. plyr will handle a list or a vector as an "l" for us
+  # we want a simple array of scores back, so we use "l" + "a" + "ply" = laply:
+  scores = laply(sentences, function(sentence, pos.words, neg.words) {
+    
+    # clean up sentences with R's regex-driven global substitute, gsub():
+    sentence = gsub('[[:punct:]]', '', sentence)
+    sentence = gsub('[[:cntrl:]]', '', sentence)
+    sentence = gsub('\\d+', '', sentence)
+    # and convert to lower case:
+    sentence = tolower(sentence)
+    
+    # split into words. str_split is in the stringr package
+    word.list = str_split(sentence, '\\s+')
+    # sometimes a list() is one level of hierarchy too much
+    words = unlist(word.list)
+    
+    # exclude stop words
+    check <- match(words,exc.words)
+    exc.list <-!is.na(check)
+    words <-words[!exc.list]
+    
+    # compare our words to the dictionaries of positive & negative terms
+    pos.matches = match(words, pos.words)
+    neg.matches = match(words, neg.words)
+    
+    # match() returns the position of the matched term or NA
+    # we just want a TRUE/FALSE:
+    pos.matches = !is.na(pos.matches)
+    neg.matches = !is.na(neg.matches)
+    
+    # and conveniently enough, TRUE/FALSE will be treated as 1/0 by sum():
+    score = sum(pos.matches) - sum(neg.matches)
+    
+    return(score)
+  }, pos.words, neg.words, .progress=.progress )
+  
+  scores.df = data.frame(score=scores, text=sentences)
+  return(scores.df)
+}
+
+
+
+data.refugees<-read.csv(file ="Data/Data_refugees_nachgeladen040116.csv")
+colnames(data.refugees)
+data_for_sentiment<-data.refugees[,c(13,18)]
+#remove 
+data_for_sentiment_cleaned<-gsub("#|@|\n|/|❤|[^[:alnum:][:blank:]+\\]"," ",data_for_sentiment$caption)
+
+positive_words<-scan("positive_word_bank.txt", what = "character", comment.char=";")
+negative_words<-scan("negative_word_bank.txt", what = "character", comment.char=";")
+#exc.words - stop words
+analysis<-score.sentiment(data_for_sentiment_cleaned,positive_words,negative_words, exc.words=NULL)
+table(analysis$score)
+hist(analysis$score)
+
+##################################################################################
+library(httr)
+#API Sentiment Analysis
+text<-data_for_sentiment_cleaned[13]
+url <- "https://community-sentiment.p.mashape.com/text/"
+r <- POST(url, add_headers("X-Mashape-Key:"="b046W1AuLimshPLIBGkMKeHGwYw0p1RCYLJjsnyHjhot6bIwCy", 
+                          "Content-Type:"="application/x-www-form-urlencoded",
+                          "Accept:"="application/json"), query=list("txt"="Hallo bad bad bad"), accept_json())
+
+
+install.packages("JSON")
+http_status(r)
+str(content(r))
+content(r, "text")
+
+
+
+
+
+
+
+
+
+# End Sentiment Analysis
+##################################################################################
+
+
+#######some other shit
+
+
 ##################################################################################
 caption<-data.refugees$caption
 a<-strsplit(as.character(caption[1:5]),"(?<=[#])",perl = TRUE)
@@ -36,6 +126,11 @@ require("stringr")
 #refs<-sapply(HashTags[1:10], function(x) sum(grepl("refugees", strsplit(x = as.character(x), split = " ", perl = TRUE))))
 #refs.welcome<-sapply(HashTags, function(x) sum(grepl("regugeeswelcome", strsplit(x = as.character(x), split = " ", perl = TRUE))))
 #refs.not.welcome<-sapply(HashTags, function(x) sum(grepl("refugeesnotwelcome|norefugees|überfremdung|wakeupeurope|nomorerefugees", strsplit(x = as.character(x), split = " ", perl = TRUE))))
+
+##################################################################################
+
+
+
 
 
 
--- main.R
+++ main.R
@@ -27,11 +27,8 @@ load("../my_oauth")
 data.refugees <- searchInstagram(tag="refugees", token=my_oauth, n = 200000)
 
 write.csv(data.refugees, file = "Data/Data_refugees.csv")
-#read data
-#data.refugees<-read.csv(file ="Data/Data_refugees.csv")
-data.refugees[,1]<-NULL
 
-#tagsToLoad<-c("refugeesnotwelcome","norefugees","flüchtling","überfremdung", "refugees","wakeupeurope", "norefugees","refugeecrisis","nomorerefugees","syrianrefugees")
+#tagsToLoad<-c("refugeesnotwelcome","norefugees","flüchtling","überfremdung", "wakeupeurope", "norefugees","refugeecrisis","nomorerefugees","syrianrefugees")
 tagsToLoad<-c("norefugees","refugeecrisis","nomorerefugees","syrianrefugees")
 for (i in 1:length(tagsToLoad)){
   print(tagsToLoad[i])
@@ -52,6 +49,49 @@ require("caret")
 idizies<-createDataPartition(y = data.with.GeoLocation$X, p=0.2, list=FALSE)
 random.data.twentypercent<-data.with.GeoLocation[idizies,]
 write.csv(data.refugees, file = "Random 20 percent.csv")
+
+##################################################################################
+#Hashtags reorganization/separation - tag / line
+##################################################################################
+#read data
+data.refugees<-read.csv(file ="Data/Data_refugees_nachgeladen040116.csv")
+data.refugees[,1]<-NULL
+
+#take only id and tags
+data_if_tag<-data.refugees[,c(13,17)]
+
+#list of list of tags
+tags<-apply(data_if_tag,1, function(x) sapply(strsplit(as.character(x[1])," "),function(y) unlist(lapply(y, function(a) unlist(a))) ) )
+#tags2<-apply(data_if_tag,1, function(x) sapply(split(as.character(x[1])," "),function(y) unlist(lapply(y, function(a) unlist(a))) ) )
+#replace NULLs with NAs
+for(i in 1:length(tags)){
+  tags[i][sapply(tags[[i]], is.null)]<-NA
+}
+id<-data_if_tag[,2]
+ids<-unlist(sapply(1:length(id),function(i) rep(as.character(id[[i]]),length(tags[[i]])) ))
+tagss<-unlist(tags)
+id_tag_pro_line<-data.frame(ids, tagss)
+
+write.csv(id_tag_pro_line, file = "Data/Data_refugees_tag_pro_line.csv")
+
+
+##################################################################################
+#Frequency Analysis
+ftable(data)
+xtabs(formula = ~ y , data)
+tab<- table(data$y)
+freq<-sort(tab, decreasing = TRUE)
+
+#data<-data[-which(is.na(data$y)),]
+positiv_id<-data[data$y=="refugeeswelcome",]$x
+negativ_id<-data[data$y=="refugeesnotwelcome",]$x
+positiv_id<-positiv_id[-which(is.na(positiv_id))]
+negativ_id<-negativ_id[-which(is.na(negativ_id$y)),]
+
+data$x[positiv_id]
+
+freq_positiv<-sort(table(positiv$y), decreasing = TRUE)
+freq_negativ<-sort(table(negativ), decreasing = TRUE)
 
 
 
